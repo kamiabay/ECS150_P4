1bc3dbd1ec52fed83f0affc269850cc742e8c5e1
Friendofmario <32601042+Friendofmario@users.noreply.github.com>
2020-02-26 16:33:34 -0800

    Seg fault at line 217, ls works, create is fixed. I did what I can to make it neater

--- p4/libfs/fs.c
+++ p4/libfs/fs.c
@@ -46,15 +46,10 @@ struct __attribute__((packed)) Disk
 	int open[128];
 };
 
-// Global Disk
-disk mainDisk = NULL;
-
-// Array for open files
-
+disk mainDisk = NULL; // Global Disk
 
 static void readSuper()
 {
-
 	Sblok superblock = malloc(BLOCK_SIZE);
 	if (block_read(0,superblock) == -1){
 		printf("read this failed\n");
@@ -76,7 +71,7 @@ static void readFAT()
 
 static void readRoot()
 {
-	root Root = malloc(BLOCK_SIZE * sizeof(uint8_t));
+	root Root = malloc(128 * sizeof(struct RootEntry));
 	block_read(mainDisk->superblock->rootIndex, Root);
 	mainDisk->rootDir = Root;
 }
@@ -95,71 +90,41 @@ int fs_mount(const char *diskname)
 		perror("open");
 		return -1;
 	}
-
-	// Allocate the disk
-	mainDisk = malloc(sizeof(struct Disk));
-
-	// Read the superblock
-	readSuper();
-
+	mainDisk = malloc(sizeof(struct Disk)); // Allocate the disk
+	readSuper(); // Read the superblock
 	if (strncmp("ECS150FS", mainDisk->superblock->signature,8))
 	{
 		perror("open");
 		return -1;
 	}
-
-	// Get superblock contents
-	if (mainDisk->superblock->numTotalBlocks != block_disk_count())
+	if (mainDisk->superblock->numTotalBlocks != block_disk_count()) // Get superblock contents
 	{
 		perror("open");
 		return -1;
 	}
-
-	// Get the FAT table
-	readFAT();
-
-	// Make first FAT block FAT_EOC
-	mainDisk->fat[0] = FAT_EOC;
-
-	// Get the root directory
-	readRoot();
-
-	// All files start off as closed
-	for(int i = 0; i < FS_FILE_MAX_COUNT; i++) {
-		mainDisk->open[i] = 0;
-	}
-
+	readFAT(); // Get the FAT table
+	mainDisk->fat[0] = FAT_EOC; // Make first FAT block FAT_EOC
+	readRoot(); // Get the root directory
+	memset(mainDisk->open, 0 , sizeof(mainDisk->open)); // All files start off as closed
 	return 0;
 }
 
 int fs_umount(void)
 {
-	/* TODO: Phase 1 */
-	// Check all files are closed
-	for(int i = 0; i < FS_FILE_MAX_COUNT; i++) {
+	for(int i = 0; i < FS_FILE_MAX_COUNT; i++) { // Check all files are closed
 		if (mainDisk->open[i] == 1) {
 			return -1;
 		}
 	}
-
-	// Write the superblock back
-	block_write(0,mainDisk->superblock);
-
-	//Write the FAT blocks back
-	for(int i = 1; i < mainDisk->superblock->numFATblocks + 1; i++)
+	block_write(0,mainDisk->superblock); // Write the superblock back
+	for(int i = 1; i < mainDisk->superblock->numFATblocks + 1; i++) //Write the FAT blocks back
 	{
 		block_write(i, mainDisk->fat);
 	}
-
-	// Write root block back
-	block_write(mainDisk->superblock->rootIndex, mainDisk->rootDir);
-
-	// Delete the block from the disk
-	free(mainDisk);
+	block_write(mainDisk->superblock->rootIndex, mainDisk->rootDir); // Write root block back
+	free(mainDisk); // Delete the block from the disk
 	mainDisk = NULL;
-
-	// Close the disk
-	if (block_disk_close() == -1)
+	if (block_disk_close() == -1) // Close the disk
 	{
 		perror("close");
 	}
@@ -168,7 +133,6 @@ int fs_umount(void)
 
 int fs_info(void)
 {
-	/* TODO: Phase 1 */
 	printf("FS Info\n");
 	printf("total_blk_count= %i\n",mainDisk->superblock->numTotalBlocks);
 	printf("fat_blk_count= %i\n",mainDisk->superblock->numFATblocks);
@@ -204,58 +168,25 @@ static int freeSpace()
 
 int fs_create(const char *filename)
 {
-	/* TODO: Phase 2 */
-
-	// Get the slot for this file
-	int space = freeSpace();
-
-	// Return -1 if we have no free space
-	if (space == -1){
+	int space = freeSpace(); // Find and get the slot for this file
+	if (space == -1){ // Return -1 if we have no free space
 		printf("Out of space\n");
 		return -1;
 	}
-
-	// Check the length of the file
-	if (sizeof(filename) >= FS_FILENAME_LEN) {
+	if (sizeof(filename) >= FS_FILENAME_LEN) { // Check the length of the file
 		printf("This disk aint big enough for that long ass name\n");
 		return -1;
 	}
-
-	// Check if there is a file of the same name
-	for(int i = 0; i < FS_FILE_MAX_COUNT; i++) {
+	for(int i = 0; i < FS_FILE_MAX_COUNT; i++) { // Check if there is a file of the same name
 			if (!strcmp(filename, mainDisk->rootDir[i].Filename)) {
 				printf("Copied\n");
 				return -1;
 		}
 	}
-
-	// Make start index FAT_EOC
-	mainDisk->rootDir[space].FirstIndex = FAT_EOC;
-
-	// Read filename into the freespace
-//	for (int i = 0; i < FS_FILENAME_LEN; i++)
-		strcpy(mainDisk->rootDir[space].Filename, filename);
-		//mainDisk->rootDir[space].Filename[i] = filename[i];
-
-	// Initialize the size to zero
-	mainDisk->rootDir[space].Filesize = 0;
-
-	// Give superblock updated root directory
-	block_write(mainDisk->superblock->rootIndex, mainDisk->rootDir);
-
-	// Find FAT space
-	while (mainDisk->fat[mainDisk->rootDir[space].FirstIndex] != 0) {
-		mainDisk->rootDir[space].FirstIndex++;
-		if (mainDisk->rootDir[space].FirstIndex) { // No space on FAT
-			return -1;
-		}
-	}
-	mainDisk->fat[mainDisk->rootDir[space].FirstIndex] = FAT_EOC;
-
-	// Give disk updated FAT
-	writeFAT();
-
-
+	mainDisk->rootDir[space].FirstIndex = FAT_EOC; // Make start index FAT_EOC
+		strcpy(mainDisk->rootDir[space].Filename, filename); // Read filename into the freespace
+	mainDisk->rootDir[space].Filesize = 0; // Initialize the size to zero
+	block_write(mainDisk->superblock->rootIndex, mainDisk->rootDir); // Give superblock updated root directory
 	return 0;
 }
 
@@ -271,32 +202,19 @@ static int findFile(const char *filename)
 
 int fs_delete(const char *filename)
 {
-	/* TODO: Phase 2 */
-
-	//Get index in root directory
-	int fileInd = findFile(filename);
-
-	// Make sure filename exists
-	if (fileInd == -1) {
+	int fileInd = findFile(filename); //Get index in root directory
+	if (fileInd == -1) { // Make sure filename exists
 		return -1;
 	}
-
-	// Make sure file is closed
-	if(mainDisk->open[fileInd] == 1) {
+	if(mainDisk->open[fileInd] == 1) { // Make sure file is closed
 		return -1;
 	}
-
-	// Delete
-	mainDisk->rootDir[fileInd].Filesize = 0;
-
-	// Remove the filename
-	for(int i = 0; i < FS_FILENAME_LEN; i++) {
+	mainDisk->rootDir[fileInd].Filesize = 0; // Delete
+	for(int i = 0; i < FS_FILENAME_LEN; i++) { // Remove the filename
 		mainDisk->rootDir[fileInd].Filename[i] = '\0';
 	}
-
-	// Erase entry from FAT table
-	int index = mainDisk->rootDir[fileInd].FirstIndex;
-	int entry = mainDisk->fat[index];
+	int index = mainDisk->rootDir[fileInd].FirstIndex; // Erase entry from FAT table
+	int entry = mainDisk->fat[index]; // Seg fault occur here
 	int oldentry;
 	while(mainDisk->fat[index] != FAT_EOC) {
 		oldentry = entry;
@@ -304,9 +222,7 @@ int fs_delete(const char *filename)
 		mainDisk->fat[oldentry] = 0;
 	}
 	mainDisk->fat[index] = 0;
-
-	// Update it to disk
-	writeFAT();
+	writeFAT(); // Update it to disk
 
 
 	return 0;
@@ -314,14 +230,14 @@ int fs_delete(const char *filename)
 
 int fs_ls(void)
 {
-	/* TODO: Phase 2 */
 	printf("FS Ls:\n");
 	for(int i = 0; i < FS_FILE_MAX_COUNT; i++) {
 		if (mainDisk->rootDir[i].Filename[0] != '\0') {
-			printf("%s ",mainDisk->rootDir[i].Filename );
+			printf("file: %s, size: %i, data_blk: %i\n",mainDisk->rootDir[i].Filename,
+													 mainDisk->rootDir[i].Filesize,
+													 mainDisk->rootDir[i].FirstIndex );
 		}
 	}
-	printf("\n");
 	return 0;
 }
 
